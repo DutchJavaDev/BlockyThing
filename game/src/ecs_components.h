d5bf39a31afeb09579fd978d958aad8da6ae1294
Boris <Borismulder@outlook.com>
2023-01-30 02:04:56 +0100

    refactor

--- game/src/ecs_components.h
+++ game/src/ecs_components.h
@@ -38,3 +38,81 @@ typedef struct
 {
 	int id;
 } DynamicBody;
+
+ECS_COMPONENT_DECLARE(Player);
+ECS_COMPONENT_DECLARE(Velocity);
+ECS_COMPONENT_DECLARE(WorldPosition);
+ECS_COMPONENT_DECLARE(Shape);
+ECS_COMPONENT_DECLARE(ShapeColor);
+ECS_COMPONENT_DECLARE(StaticBody);
+ECS_COMPONENT_DECLARE(DynamicBody);
+
+#define MAX_ENTITY_COUNT 456
+const ecs_entity_t* Entityies[MAX_ENTITY_COUNT];
+int nextEntityIndexCount = 0;
+
+static ecs_world_t* initBlockyThingWorld()
+{
+	ecs_world_t* world = ecs_init();
+
+	ECS_COMPONENT_DEFINE(world, Player);
+	ECS_COMPONENT_DEFINE(world, Velocity);
+	ECS_COMPONENT_DEFINE(world, WorldPosition);
+	ECS_COMPONENT_DEFINE(world, Shape);
+	ECS_COMPONENT_DEFINE(world, ShapeColor);
+	ECS_COMPONENT_DEFINE(world, StaticBody);
+	ECS_COMPONENT_DEFINE(world, DynamicBody);
+
+	return world;
+}
+
+static void AddStaticEntityAt(ecs_world_t* world, float x, float y, float width, float height)
+{
+	if (nextEntityIndexCount > MAX_ENTITY_COUNT)
+		return;
+
+	Entityies[nextEntityIndexCount] = ecs_new_id(world);
+
+	Color c;
+	c.r = GetRandomValue(0, 255);
+	c.g = GetRandomValue(0, 255);
+	c.b = GetRandomValue(0, 255);
+	c.a = 255;
+
+	int shape = GetRandomValue(0, 2);
+
+	ecs_set(world, Entityies[nextEntityIndexCount], WorldPosition, { x,y });
+	ecs_set(world, Entityies[nextEntityIndexCount], StaticBody, { 0 });
+	ecs_set(world, Entityies[nextEntityIndexCount], Shape, { shape,width,height });
+	ecs_set(world, Entityies[nextEntityIndexCount], ShapeColor, { c });
+
+	nextEntityIndexCount++;
+}
+
+static void AddDynamicEntityAt(ecs_world_t* world, float x, float y, float width, float height, bool isPlayer)
+{
+	if (nextEntityIndexCount > MAX_ENTITY_COUNT)
+		return;
+
+	Entityies[nextEntityIndexCount] = ecs_new_id(world);
+
+	Color c;
+	c.r = GetRandomValue(0, 255);
+	c.g = GetRandomValue(0, 255);
+	c.b = GetRandomValue(0, 255);
+	c.a = 255;
+
+	int shape = GetRandomValue(0, 2);
+
+	ecs_set(world, Entityies[nextEntityIndexCount], WorldPosition, { x,y });
+	ecs_set(world, Entityies[nextEntityIndexCount], DynamicBody, { 0 });
+	ecs_set(world, Entityies[nextEntityIndexCount], Shape, { shape,width,height });
+	ecs_set(world, Entityies[nextEntityIndexCount], ShapeColor, { c });
+
+	if (isPlayer)
+	{
+		ecs_set(world, Entityies[nextEntityIndexCount], Player, { 0 });
+	}
+
+	nextEntityIndexCount++;
+}
--- game/src/ecs_systems.h
+++ game/src/ecs_systems.h
@@ -9,3 +9,17 @@ void RenderWorld(ecs_iter_t* it);
 void RenderPosition(ecs_iter_t* it);
 void RenderHitBox(ecs_iter_t* it);
 void EndRendering(ecs_iter_t* it);
+
+
+static void initSystems(ecs_world_t* world)
+{
+	ECS_SYSTEM(world, CheckUserInput, EcsPreUpdate, Player, Velocity, DynamicBody);
+	ECS_SYSTEM(world, ApplyVelocity, EcsOnUpdate, Velocity, WorldPosition, DynamicBody);
+	ECS_SYSTEM(world, CollisionDetection, EcsOnValidate, WorldPosition, Shape, Velocity, DynamicBody);
+	ECS_SYSTEM(world, UpdatePlayerCamera, EcsPostUpdate, WorldPosition, Shape, Player);
+	ECS_SYSTEM(world, BeginRendering, EcsPreStore);
+	ECS_SYSTEM(world, RenderWorld, EcsOnStore, WorldPosition, Shape, ShapeColor);
+	ECS_SYSTEM(world, RenderPosition, EcsOnStore, WorldPosition, Shape);
+	ECS_SYSTEM(world, RenderHitBox, EcsOnStore, WorldPosition, Shape, StaticBody);
+	ECS_SYSTEM(world, EndRendering, EcsOnStore);
+}
