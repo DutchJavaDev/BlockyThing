5ab2614d876c66e588be2c8b6c8003433108c440
Boris <Borismulder@outlook.com>
2023-02-24 14:54:31 +0100

    load tiles + collision data into ecs

--- game/src/game_init.h
+++ game/src/game_init.h
@@ -1,19 +1,4 @@
 #pragma once
-#pragma region MapLayersId
-#define WALKALBE_PATH_LAYER 1
-#define PLAYER_LAYER 5
-#define NATURE_OBJECTS_LAYER 4
-#define OBJECTS_LAYER 2
-#pragma endregion
-
-#pragma region InternalFunctions
-#pragma endregion
-
-
-#define FILE_LINEBUFFER 32
-#define MAX_WORLD_OBJECTS 64
-
-static const float hitBoxMultiplier = 1.0f;
 static const float MAX_ZOOM = 7.0f;
 static const int WORLD_SPEED = 10;
 
@@ -76,46 +61,6 @@ typedef struct {
 	int height;
 }CollisionObject;
 
-WorldObject Objects[MAX_WORLD_OBJECTS];
-
-static Texture2D ResolveTexture(int index)
-{
-	switch (index)
-	{
-	case 0:
-		return worldTileSet;
-
-	default:
-		return worldTileSet; // :)
-	}
-}
-
-static int GetTileSize(int index)
-{
-	switch (index)
-	{
-	case 0:
-		return 32;
-	case 1:
-		return 16;
-
-	default:
-		return 64; // :)
-	}
-}
-
-static void SetPlayerCamera(int halfScreenWidth, int halfScreenHeight)
-{
-	playerCamera.offset = (Vector2)
-	{
-		halfScreenWidth,
-		halfScreenHeight
-	};
-	playerCamera.target = (Vector2){ 75,75 };
-	playerCamera.zoom = 2.0f;
-	playerCamera.rotation = 0;
-}
-
 static void LoadWorld(ecs_world_t* ecs_world)
 {
 	if (ChangeDirectory("resources"))
@@ -138,14 +83,11 @@ static void LoadWorld(ecs_world_t* ecs_world)
 
 		while (fread(&world, sizeof(WorldFile), 1, worldFile))
 		{
-			worldWidth = world.width;
-			worldHeight = world.height;
 			tileWidth = world.tileWidth;
 			tileHeight = world.tileHeigt;
 
-			tileMapTexture = LoadRenderTexture(worldWidth * tileWidth, worldHeight * tileHeight);
-			SetTextureFilter(tileMapTexture.texture, TEXTURE_FILTER_POINT);
-			BeginTextureMode(tileMapTexture);
+			worldWidth = world.width * tileWidth;
+			worldHeight = world.height * tileHeight;
 
 			FILE* textTureInfoFile;
 			textTureInfoFile = fopen("auril_home.tf.0.tfn.bin", "rb");
@@ -172,6 +114,10 @@ static void LoadWorld(ecs_world_t* ecs_world)
 			}
 
 			WorldTile tile = { 0 };
+			tileMapTexture = LoadRenderTexture(worldWidth, worldHeight);
+			SetTextureFilter(tileMapTexture.texture, TEXTURE_FILTER_POINT);
+			BeginTextureMode(tileMapTexture);
+			ClearBackground(PINK);
 
 			while (fread(&tile, sizeof(WorldTile), 1, tilemapFile))
 			{
@@ -183,7 +129,6 @@ static void LoadWorld(ecs_world_t* ecs_world)
 
 				int tileX = tile.xPosition * tileWidth;
 				int tileY = tile.yPosition * tileHeight;
-
 
 				// Load texture data
 				TexturePosition textureLocation = { 0 };
@@ -202,7 +147,16 @@ static void LoadWorld(ecs_world_t* ecs_world)
 				{
 					if (cob.tileId == tile.id)
 					{
-						DrawRectangle(tileX + cob.x, tileY + cob.y, cob.width, cob.height, RED);
+						// Add as static body
+						ecs_entity_t* static_body = ecs_new_id(auril_world);
+
+						int x = tileX + cob.x;
+						int y = tileY + cob.y;
+						int width = cob.width;
+						int height = cob.height;
+
+						ecs_set(auril_world, static_body, WorldPosition, { x, y });
+						ecs_set(auril_world, static_body, StaticBody, { tile.id, x + width / 2,y + height / 2,width,height });
 					}
 				}
 
@@ -219,7 +173,7 @@ static void LoadWorld(ecs_world_t* ecs_world)
 		ecs_entity_t* player = ecs_new_id(ecs_world);
 		ecs_set(ecs_world, player, Player, { 0 });
 		ecs_set(ecs_world, player, Velocity, { 0, 0 });
-		ecs_set(ecs_world, player, WorldPosition, { 0,0 });
-		ecs_set(ecs_world, player, DynamicBody, { 0,150,150,tileWidth,tileHeight });
+		ecs_set(ecs_world, player, WorldPosition, { teleX,teleY });
+		ecs_set(ecs_world, player, DynamicBody, { 0,150,150,16,16 });
 	}
 }
