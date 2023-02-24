ab7ed3c63212972c90845342093ed69deebe514f
Boris <Borismulder@outlook.com>
2023-02-25 00:14:28 +0100

    temp world map

--- resources/world_tiles.tsx
+++ resources/world_tiles.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="world_tiles" tilewidth="32" tileheight="32" tilecount="33" columns="3">
- <image source="world_tiles.png" width="96" height="383"/>
+<tileset version="1.9" tiledversion="1.9.2" name="world_tiles" tilewidth="32" tileheight="32" tilecount="45" columns="3">
+ <image source="world_tiles.png" width="96" height="480"/>
  <tile id="18">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="5" height="32.25"/>
@@ -43,6 +43,16 @@
   <objectgroup draworder="index" id="2">
    <object id="1" x="0.0909091" y="27" width="32" height="5"/>
    <object id="2" x="27" y="0" width="5" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="36">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="7.9375" y="9.625" width="16.1875" height="13.5625"/>
+  </objectgroup>
+ </tile>
+ <tile id="37">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4.59375" y="7.21875" width="22" height="17.3125"/>
   </objectgroup>
  </tile>
 </tileset>
